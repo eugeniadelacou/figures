263346875b9173f9a2f8d48b6de5127164fd609e
Eugénia Delacou <Eugenia@airdeeugenia2.home>
2022-01-13 15:07:19 +0000

    changes to the t^1/2 axis

--- python_figures.py
+++ python_figures.py
@@ -26,13 +26,12 @@ def dD3(liste_dD3, feuille, size):
 
 #Annotate the points with a line, arrow and text
 def line_annotate(axe, liste, x, y, xtext, ytext, text):
-    axe.plot([x,x], [y, liste.min()-1], color='black',
-    linewidth=1.5, linestyle='--')
+    axe.plot([x,x], [y, liste.min()-1], color='black', linewidth=1.5, linestyle='--')
     # x,y -> where the arrow points at
     # xtext, ytext -> where the text is
     axe.annotate(text, xy=(x, y), xycoords='data', xytext=(xtext, ytext),
-    textcoords='offset points', fontsize=10,
-    arrowprops=dict(arrowstyle="->", connectionstyle="arc"))
+    textcoords='offset points', fontsize=10, arrowprops=dict(arrowstyle="->",
+    connectionstyle="arc"))
     return
 
 
@@ -45,13 +44,15 @@ def tick(liste_time_min, min_tick):
     else:
         plt.xticks(range(0,int(last_tick),min_tick))
 
-#returns the values of sqrt(t) and df3/3 from 11 to 60 min
+#returns the values of sqrt(t) and df3/3 from 6 or 11 to 60 min
+# * to modify *
 def prepare_data_fit(liste_sqrt, liste_df3_fit, liste_time_min, liste_df3, feuille, size):
+#    j = 0
     for i in range(size):
-        if i < size - 11 and (11 <= liste_time_min[i+11] <= 60):
-            liste_sqrt[i] = -math.sqrt(feuille['Time_1 [s]'].iloc[i]/60)
-        else:
-            liste_sqrt[i] = None
+        liste_sqrt[i] = None
+        if 11 <= liste_time_min[i] <= 60:
+            liste_sqrt[i] = -math.sqrt(liste_time_min[i]-11)
+#            j=j+1
 
         if liste_time_min[i] <=60:
             liste_df3_fit[i] = liste_df3[i]
@@ -67,7 +68,10 @@ if __name__ == "__main__":
 
     #opens sheet_name and stores everything in feuille
     feuille = pd.read_excel(sys.argv[1], sheet_name=sys.argv[2])
-    title = sys.argv[3]
+    title = ""
+    for i in range(3, len(sys.argv)):
+        title = " ".join([title, sys.argv[i]])
+    #title = sys.argv[3]
     size = len(feuille)-1
 
     liste_time_min = np.zeros(size)
@@ -86,6 +90,7 @@ if __name__ == "__main__":
 
     axis[0].set_ylabel('∆f$_3$/3 (Hz)', fontsize=16, color='black')
     axis[0].set_xlabel('Time (min)', fontsize=16)
+
     # annotation coordinates for PVP
     # * to modify *
     x1, y1 = 10, 1
@@ -98,29 +103,28 @@ if __name__ == "__main__":
 
     #Annotate the points with a line: e.g. 10 and 60 min
     line_annotate(axis[0], liste_df3, x1, y1, xtext1, ytext1, r'PVP')
-    line_annotate(axis[0], liste_df3, x2, y2, xtext2, ytext2, r'rinsing')
+    #line_annotate(axis[0], liste_df3, x2, y2, xtext2, ytext2, r'rinsing')
 
     #Draw a box on the part which is fitted below
     axis[0].plot([0, 60, 60, 0, 0], [0.5, 0.5, liste_df3.min(), liste_df3.min(), 0.5],
-    color='blue', linewidth=1, linestyle='-')
+    color = 'blue', linewidth = 1, linestyle = '-')
 
     #Add an arrow pointing downwards
-    axis[0].annotate("", (30, liste_df3.min()), xycoords=('data'), xytext=(30,
+    axis[0].annotate("", (30, liste_df3.min()), xycoords = ('data'), xytext=(30,
     liste_df3.min()-5),
-    textcoords='data', fontsize=10,
-    arrowprops=dict(arrowstyle="<-", connectionstyle="arc"))
+    textcoords = 'data', fontsize = 10, arrowprops = dict(arrowstyle = "<-",
+    connectionstyle = "arc"))
 
     #first plot, second axis: ∆D3 = f(t)
     ax2 = axis[0].twinx()
     ax2.plot(time_min(liste_time_min, feuille, size), dD3(liste_dD3, feuille,
-    size), color='red')
-    ax2.set_ylabel('∆D$_3$ (ppm)', fontsize=16, color='red')
+    size), color = 'red')
+    ax2.set_ylabel('∆D$_3$ (ppm)', fontsize = 16, color = 'red')
 
-    ax2.set_title(title , fontsize=20)
+    ax2.set_title(title , fontsize = 20)
 
     #Use Latex to set tick labels
     tick(liste_time_min, 30)
-
 
     #second plot : ∆f3/3 = f(t) with t^1/2 fit
     #get data
@@ -128,19 +132,23 @@ if __name__ == "__main__":
     liste_time_min, liste_df3, feuille, size)
 
     #plot ∆f3/3 = f(t) until 60 min
-    axis[1].plot(liste_time_min, liste_df3_fit, color='black', linestyle='none',
-    marker='s', markersize=0.5)
-    axis[1].set_ylabel('∆f$_3$/3 (Hz)', fontsize=16, color='black')
-    axis[1].set_xlabel('Time (min)', fontsize=16)
+    axis[1].plot(liste_time_min, liste_df3_fit, color = 'black', linestyle = 'none',
+    marker = 's', markersize = 0.5)
+    axis[1].set_ylabel('∆f$_3$/3 (Hz)', fontsize = 16, color = 'black')
+    axis[1].set_xlabel('Time (min)', fontsize = 16)
 
     #plot t^1/2 = f(t)
     ax3 = axis[1].twinx()
-    ax3.plot(liste_time_min, liste_sqrt, color='red',linestyle='--',
-    label='-$\sqrt{t}$')
+    limite_y = axis[1].get_ylim()
+
+    ax3.plot(liste_time_min, liste_sqrt, color = 'red',linestyle = '--',
+    label = '-$\sqrt{t}$')
+    ax3.set_ylim(limite_y)
+
 
     ax3.get_yaxis().set_visible(False)
     ax3.legend()
-    ax3.set_title("Adsorption model fit")
+    axis[1].set_title("Adsorption model fit")
 
     plt.tight_layout()
 
